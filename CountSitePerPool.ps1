ad73bbeed933898dc3f514dcf75b79a3458eaa1f
patresepinheiro <47859374+patresepinheiro@users.noreply.github.com>
2019-02-21 11:42:39 -0300

    Create CountSitePerPool.ps1
    Script elaborado para realizar a contagem de sites ativos em um determinado pool. 
    Web Service utilizado: IIS6

---
+++ CountSitePerPool.ps1
@@ -0,0 +1,11 @@
+# Desenvolvido por: Patrese Renan
+# Programa: CountSitePerPool - IIS 6
+# Versão: 1.0.0 
+# Criação: 1.0.0 19/02/2018 (Script Básico)
+
+#Variável - Nome do Processo
+$processname = "w3wp.exe"
+
+#Consulta WMI
+Get-WmiObject Win32_Process -Filter "name = '$processname'" | Select-Object CommandLine | Where {$_ -match "DefaultAppPool"} | 
+measure-object | Select-Object Count -ExpandProperty Count
